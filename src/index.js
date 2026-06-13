5b79f0bac1ae534aa4cf3a0fa77792fde53495bf
asunder <trashemail3301@gmail.com>
2026-06-13 13:41:50 -0400

    Add listener and functionality to button

--- src/index.js
+++ src/index.js
@@ -3,3 +3,6 @@ import "./modern-normalize.css";
 import { handleDOM } from "./modules/DOM.js";
 
 handleDOM.createProject();
+
+const button = document.getElementById("add-btn");
+button.addEventListener("click", handleDOM.createProject);
