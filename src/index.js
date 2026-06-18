c397df5cd6d8a8fc66b34453b0826854bb0f65d0
asunder <trashemail3301@gmail.com>
2026-06-18 08:20:26 -0400

    Push projects to an array containing all projects

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import "./modern-normalize.css";
 import { DOMHandler, DOMRenderer } from "./modules/DOM.js";
 import { Project } from "./modules/project.js";
 
+const myProjects = [];
+
 // Initialize default data structure configuration
 const defaultProject = new Project("Default");
 DOMRenderer.createProjectCard("project-container", "Default", defaultProject);
+myProjects.push(defaultProject);
 
 const form = document.getElementById("project-form");
 const dialog = document.getElementById("create-project");
@@ -17,9 +20,11 @@ form.addEventListener("submit", (event) => {
     const newProject = new Project(name);
     
     DOMRenderer.createProjectCard("project-container", name, newProject);
+    myProjects.push(newProject);
 
     form.reset();
     dialog.close();
 
     console.log(newProject);
+    console.log(myProjects);
 });
