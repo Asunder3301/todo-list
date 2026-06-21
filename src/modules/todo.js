1ff61e69a5fa2edcd939446da445c0c9446521e8
asunder <trashemail3301@gmail.com>
2026-06-21 11:25:55 -0400

    Formate due dates using date-fns enUS locate

--- src/modules/todo.js
+++ src/modules/todo.js
@@ -1,8 +1,11 @@
+import { format } from "date-fns";
+import { enUS } from "date-fns/locale";
+
 export class Todo {
     constructor(title, description, dueDate, priority) {
             this.title = title;
             this.description = description;
-            this.dueDate = dueDate;
+            this.dueDate = format(new Date(dueDate), 'MMM-dd-yyyy', { locale: enUS });
             this.priority = priority;
     }
 }
